6020dfd2a3c4eca3e560aa1be941e5815507fd33
Tyler Greer <tgreer812@gmail.com>
2024-05-19 15:09:25 -0400

    Ready for deployment
    Working and ready for deployment to the cloud

---
+++ CS2ServerConfig.cs
@@ -0,0 +1,106 @@
+﻿using Azure.ResourceManager.ContainerInstance.Models;
+using System.Collections.Generic;
+
+namespace Tyler.Greer
+{
+    public class CS2ServerConfig
+    {
+        public string SrcdsToken { get; set; } // Game Server Token from https://steamcommunity.com/dev/managegameservers
+        public string ServerName { get; set; } // Set the visible name for your private server
+        public string ServerPassword { get; set; } // CS2 server password
+        public string Cheats { get; set; } // 0 - disable cheats, 1 - enable cheats
+        public string Port { get; set; } // CS2 server listen port tcp_udp
+        public string RconPort { get; set; } // Use a simple TCP proxy to have RCON listen on an alternative port
+        public string MaxPlayers { get; set; } // Max players
+        public string AdditionalArgs { get; set; } // Optional additional arguments to pass into cs2
+        public string GameAlias { get; set; } // Game type, e.g. casual, competitive, deathmatch
+        public string GameType { get; set; } // Used if CS2_GAMEALIAS not defined
+        public string GameMode { get; set; } // Used if CS2_GAMEALIAS not defined
+        public string MapGroup { get; set; } // Map pool
+        public string StartMap { get; set; } // Start map
+        public string BotDifficulty { get; set; } // 0 - easy, 1 - normal, 2 - hard, 3 - expert
+        public string BotQuota { get; set; } // Number of bots
+        public string BotQuotaMode { get; set; } // fill, competitive
+        public string TvAutoRecord { get; set; } // Automatically records all games as CSTV demos: 0=off, 1=on
+        public string TvEnable { get; set; } // Activates CSTV on server: 0=off, 1=on
+        public string TvPort { get; set; } // Host SourceTV port
+        public string TvPassword { get; set; } // CSTV password for clients
+        public string TvRelayPassword { get; set; } // CSTV password for relay proxies
+        public string TvMaxRate { get; set; } // World snapshots to broadcast per second. Affects camera tickrate
+        public string TvDelay { get; set; } // Max CSTV spectator bandwidth rate allowed, 0 == unlimited
+        public string Log { get; set; } // 'on'/'off'
+        public string LogMoney { get; set; } // Turns money logging on/off: (0=off, 1=on)
+        public string LogDetail { get; set; } // Combat damage logging: (0=disabled, 1=enemy, 2=friendly, 3=all)
+        public string LogItems { get; set; } // Turns item logging on/off: (0=off, 1=on)
+
+        public CS2ServerConfig(dynamic data)
+        {
+            SrcdsToken = data?.SRCDS_TOKEN ?? Environment.GetEnvironmentVariable("SRCDS_TOKEN") ?? "";
+            if (string.IsNullOrEmpty(SrcdsToken))
+            {
+                throw new ArgumentException("SRCDS_TOKEN is required");
+            }
+
+            ServerName = data?.CS2_SERVERNAME ?? "CS2_Game_Server";
+            ServerPassword = data?.CS2_PW ?? string.Empty;
+            Cheats = data?.CS2_CHEATS ?? "0";
+            Port = data?.CS2_PORT ?? "27015";
+            RconPort = data?.CS2_RCON_PORT ?? "27050";
+            MaxPlayers = data?.CS2_MAXPLAYERS ?? "10";
+            AdditionalArgs = data?.CS2_ADDITIONAL_ARGS ?? string.Empty;
+            GameAlias = data?.CS2_GAMEALIAS ?? string.Empty;
+            GameType = data?.CS2_GAMETYPE ?? "0";
+            GameMode = data?.CS2_GAMEMODE ?? "1";
+            MapGroup = data?.CS2_MAPGROUP ?? "mg_active";
+            StartMap = data?.CS2_STARTMAP ?? "de_inferno";
+            BotDifficulty = data?.CS2_BOT_DIFFICULTY ?? string.Empty;
+            BotQuota = data?.CS2_BOT_QUOTA ?? string.Empty;
+            BotQuotaMode = data?.CS2_BOT_QUOTA_MODE ?? string.Empty;
+            TvAutoRecord = data?.TV_AUTORECORD ?? "0";
+            TvEnable = data?.TV_ENABLE ?? "0";
+            TvPort = data?.TV_PORT ?? "27020";
+            TvPassword = data?.TV_PW ?? "changeme";
+            TvRelayPassword = data?.TV_RELAY_PW ?? "changeme";
+            TvMaxRate = data?.TV_MAXRATE ?? "0";
+            TvDelay = data?.TV_DELAY ?? "0";
+            Log = data?.CS2_LOG ?? "on";
+            LogMoney = data?.CS2_LOG_MONEY ?? "0";
+            LogDetail = data?.CS2_LOG_DETAIL ?? "0";
+            LogItems = data?.CS2_LOG_ITEMS ?? "0";
+        }
+
+        public List<ContainerEnvironmentVariable> ToEnvironmentVariables()
+        {
+            return new List<ContainerEnvironmentVariable>
+            {
+                new("SRCDS_TOKEN") { Value = SrcdsToken },
+                new("CS2_SERVERNAME") { Value = ServerName },
+                new("CS2_PW") { Value = ServerPassword },
+                new("CS2_CHEATS") { Value = Cheats },
+                new("CS2_PORT") { Value = Port },
+                new("CS2_RCON_PORT") { Value = RconPort },
+                new("CS2_MAXPLAYERS") { Value = MaxPlayers },
+                new("CS2_ADDITIONAL_ARGS") { Value = AdditionalArgs },
+                new("CS2_GAMEALIAS") { Value = GameAlias },
+                new("CS2_GAMETYPE") { Value = GameType },
+                new("CS2_GAMEMODE") { Value = GameMode },
+                new("CS2_MAPGROUP") { Value = MapGroup },
+                new("CS2_STARTMAP") { Value = StartMap },
+                new("CS2_BOT_DIFFICULTY") { Value = BotDifficulty },
+                new("CS2_BOT_QUOTA") { Value = BotQuota },
+                new("CS2_BOT_QUOTA_MODE") { Value = BotQuotaMode },
+                new("TV_AUTORECORD") { Value = TvAutoRecord },
+                new("TV_ENABLE") { Value = TvEnable },
+                new("TV_PORT") { Value = TvPort },
+                new("TV_PW") { Value = TvPassword },
+                new("TV_RELAY_PW") { Value = TvRelayPassword },
+                new("TV_MAXRATE") { Value = TvMaxRate },
+                new("TV_DELAY") { Value = TvDelay },
+                new("CS2_LOG") { Value = Log },
+                new("CS2_LOG_MONEY") { Value = LogMoney },
+                new("CS2_LOG_DETAIL") { Value = LogDetail },
+                new("CS2_LOG_ITEMS") { Value = LogItems }
+            };
+        }
+    }
+}
--- CreateCS2Server.cs
+++ CreateCS2Server.cs
@@ -9,6 +9,7 @@ using Azure.ResourceManager.Resources;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Azure.Core;
+using System;
 
 namespace Tyler.Greer
 {
@@ -29,30 +30,36 @@ namespace Tyler.Greer
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic? data = JsonConvert.DeserializeObject(requestBody);
+            CS2ServerConfig serverConfig;
+            try
+            {
+                // Use the CS2ServerConfig class to handle configuration
+                serverConfig = new CS2ServerConfig(data);
+            } catch (ArgumentException e) {
+                var res = req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+                await res.WriteAsJsonAsync(new { error = e.Message });
+                return res;
+            }
 
-            // Retrieve the SRCDS_TOKEN from environment variables
-            string? srcdsToken = Environment.GetEnvironmentVariable("SRCDS_TOKEN") ?? "";
-
-            // TODO: Uncomment the following code block to validate the SRCDS_TOKEN
-            // and remove the hardcoded value above
-            // if (string.IsNullOrEmpty(srcdsToken))
-            // {
-            //     var badRequestResponse = req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
-            //     await badRequestResponse.WriteStringAsync("SRCDS_TOKEN is not configured.");
-            //     return badRequestResponse;
-            // }
-
-            // Set internal configuration
-            string serverName = "CS2_Game_Server"; // Automatically set server name
-            string cs2Port = "27015";
-            string rconPort = "27050"; // Use a different port for RCON to avoid Azure limitation
-            string cs2MaxPlayers = data?.CS2_MAXPLAYERS ?? "10";
-            string cs2Cheats = data?.CS2_CHEATS ?? "0";
-
-            string containerGroupName = "cs2containergroup";
+            // Generate a unique UUID for the container group name
+            string uuid = Guid.NewGuid().ToString();
+            string containerGroupName = $"cs2containergroup-{uuid}";
             string resourceGroupName = "tc-cs2-rg";
-            //string location = "eastus";
-            string imageName = "joedwards32/cs2"; // Assuming the image is public or accessible
+            string imageName = "joedwards32/cs2";
+
+            // Set Azure File Storage configuration
+            string mountPath = "/home/steam/cs2-dedicated";
+            string storageAccountName = "tgcs2storage";
+            string storageAccountShareName = "cs2fileshare";
+            //
+            string storageAccountKey = Environment.GetEnvironmentVariable("STORAGE_ACCOUNT_KEY") ?? string.Empty;
+            if (string.IsNullOrEmpty(storageAccountKey))
+            {
+                // If the storage account key is not set, return a 500 error
+                var res = req.CreateResponse(System.Net.HttpStatusCode.InternalServerError);
+                await res.WriteAsJsonAsync(new { error = "STORAGE_ACCOUNT_KEY is required" });
+                return res;
+            }
 
             var credentials = new DefaultAzureCredential(new DefaultAzureCredentialOptions
             {
@@ -68,48 +75,43 @@ namespace Tyler.Greer
             var containerResourceRequests = new ContainerResourceRequestsContent(2, 2);
             var containerResourceRequirements = new ContainerResourceRequirements(containerResourceRequests);
 
-            // Define container environment variables
-            var environmentVariables = new List<ContainerEnvironmentVariable>
-            {
-                new("SRCDS_TOKEN") { Value = srcdsToken },
-                new("CS2_SERVERNAME") { Value = serverName },
-                new("CS2_CHEATS") { Value = cs2Cheats },
-                new("CS2_PORT") { Value = cs2Port },
-                new("CS2_RCON_PORT") { Value = rconPort },
-                new("CS2_MAXPLAYERS") { Value = cs2MaxPlayers }
-            };
+            // Define the container using the public constructor
+            var container = new ContainerInstanceContainer(name: "cs2-container", image: imageName, resources: containerResourceRequirements);
 
-            // Define container ports
+            // Add environment variables and ports to the container instance
+            foreach (var envVar in serverConfig.ToEnvironmentVariables())
+            {
+                container.EnvironmentVariables.Add(envVar);
+            }
+
             var containerPorts = new List<ContainerPort>
             {
                 new(27015) { Protocol = ContainerNetworkProtocol.Udp },
                 new(27020) { Protocol = ContainerNetworkProtocol.Udp },
                 new(27050) { Protocol = ContainerNetworkProtocol.Tcp }
             };
+            foreach (var port in containerPorts)
+            {
+                container.Ports.Add(port);
+            }
 
-            // Define container group ports
+            // Define the volume mount for Azure File Storage
+            var volumeMount = new ContainerVolumeMount("cs2volume", mountPath);
+            container.VolumeMounts.Add(volumeMount);
+
+            // Define the Azure File volume
+            var azureFileVolume = new ContainerInstanceAzureFileVolume(storageAccountShareName, storageAccountName)
+            {
+                StorageAccountKey = storageAccountKey
+            };
+
+            // Define the container group IP address
             var containerGroupPorts = new List<ContainerGroupPort>
             {
                 new ContainerGroupPort(27015) { Protocol = ContainerGroupNetworkProtocol.Udp },
                 new ContainerGroupPort(27020) { Protocol = ContainerGroupNetworkProtocol.Udp },
                 new ContainerGroupPort(27050) { Protocol = ContainerGroupNetworkProtocol.Tcp }
             };
-
-            // Define the container using the public constructor
-            var container = new ContainerInstanceContainer(name: "cs2-container", image: imageName, resources: containerResourceRequirements);
-
-            // Add environment variables and ports to the container instance
-            foreach (var envVar in environmentVariables)
-            {
-                container.EnvironmentVariables.Add(envVar);
-            }
-
-            foreach (var port in containerPorts)
-            {
-                container.Ports.Add(port);
-            }
-
-            // Define the container group IP address
             var containerGroupIPAddress = new ContainerGroupIPAddress(containerGroupPorts, ContainerGroupIPAddressType.Public);
 
             // Define the container group
@@ -119,18 +121,33 @@ namespace Tyler.Greer
                 RestartPolicy = ContainerGroupRestartPolicy.Never
             };
 
+            // Add the volume to the container group
+            containerGroup.Volumes.Add(new ContainerVolume("cs2volume")
+            {
+                AzureFile = azureFileVolume
+            });
+
             await resourceGroup.GetContainerGroups().CreateOrUpdateAsync(WaitUntil.Completed, containerGroupName, containerGroup);
+
+            // Pull the IP address of the container group
+            var containerGroupResponse = await resourceGroup.GetContainerGroups().GetAsync(containerGroupName);
+            string resolvedIP = containerGroupResponse.Value.Data.IPAddress.IP.ToString();
 
             var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
             await response.WriteAsJsonAsync(new
             {
-                ServerName = serverName,
-                IP = "To be retrieved", // Placeholder for the IP address retrieval logic
+                ServerName = serverConfig.ServerName,
+                IP = resolvedIP,
                 Ports = new
                 {
-                    GamePort = cs2Port,
-                    TvPort = "27020"
-                }
+                    GamePort = serverConfig.Port,
+                    RconPort = serverConfig.RconPort,
+                    TvPort = serverConfig.TvPort
+                },
+#if DEBUG
+                ContainerGroupName = containerGroupName,
+                ResourceGroupName = resourceGroupName
+#endif
             });
 
             return response;
